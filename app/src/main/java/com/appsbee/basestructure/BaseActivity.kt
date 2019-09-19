27fd12f3adcfb83f9aab229a203389b1f81e1ce2
Anurag Samanta <anurag.samanta@appsbee.com>
2019-09-19 15:49:57 +0530

    initial

---
+++ app/src/main/java/com/appsbee/basestructure/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.appsbee.basestructure
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()
---
+++ app/src/main/java/com/appsbee/basestructure/BlankViewModel.kt
@@ -0,0 +1,7 @@
+package com.appsbee.basestructure
+
+import androidx.lifecycle.ViewModel
+
+class BlankViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
